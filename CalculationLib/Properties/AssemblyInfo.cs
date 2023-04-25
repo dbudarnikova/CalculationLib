f77572331ec3a8aea4f4126d04fffd2445269217
Darya <Darya@DARYA->
2023-04-25 20:27:10 +0600

    Добавьте файлы проекта.

---
+++ CalculationLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("CalculationLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("UralSOFT")]
+[assembly: AssemblyProduct("CalculationLib")]
+[assembly: AssemblyCopyright("Copyright © UralSOFT 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8919895c-2c92-4832-8dbd-f8ac5e6129d3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculationLib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 6, b = 3;
+            Console.WriteLine("a + b = {0}", CalculationLib.CalculationLib.Plus(a, b));
+            Console.WriteLine("a - b = {0}", CalculationLib.CalculationLib.Minus(a, b));
+            Console.WriteLine("a / b = {0}", CalculationLib.CalculationLib.Divide(a, b));
+            Console.WriteLine("a * b = {0}", CalculationLib.CalculationLib.Multiply(a, b));
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
